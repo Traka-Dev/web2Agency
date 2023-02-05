93b0f7a79075d4dda105bad4f3bd2c9b80bff1c1
TrakaDev <trakadev@gmail.com>
2023-02-04 21:56:45 -0600

    [ADD] Services Data

---
+++ src/data/services.js
@@ -0,0 +1,53 @@
+import icon1 from 'assets/icons/service-1-1.svg';
+import icon2 from 'assets/icons/service-1-2.svg';
+import icon3 from 'assets/icons/service-1-3.svg';
+import icon4 from 'assets/icons/service-1-4.svg';
+
+export const services_data = {
+    slogan: { en: "Quality features", es: "Características de Calidad" },
+    title: { en: "Highlighting the Value of Our Digital Solutions", es: "Destacando el Valor de Nuestras Soluciones Digitales" },
+    services: {
+        en: [{
+                icon: icon1,
+                title: 'Unlimited Customization',
+                text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
+            },
+            {
+                icon: icon2,
+                title: 'Vector shape & resizable',
+                text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
+            },
+            {
+                icon: icon3,
+                title: 'Editing freedom',
+                text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
+            },
+            {
+                icon: icon4,
+                title: 'Best Award history',
+                text: 'Get your blood tests delivered at home collect a sample from the your blood tests.',
+            },
+        ],
+        es: [{
+                icon: icon1,
+                title: 'Diseño Unico',
+                text: 'Celebrando la Creatividad y la Originalidad en Cada Proyecto.',
+            },
+            {
+                icon: icon2,
+                title: 'Transformando Ideas',
+                text: 'Converte tus ideas en soluciones impactantes y funcionalmente sólidas.',
+            },
+            {
+                icon: icon3,
+                title: 'Marketing Efectivo',
+                text: 'Lleva tu marca a un nuevo nivel con nuestras estrategias de marketing innovadoras y personalizadas.',
+            },
+            {
+                icon: icon4,
+                title: 'Entregando ROI',
+                text: 'Te brindamos soluciones que entregan un retorno de inversión real y sostenible.',
+            },
+        ]
+    }
+}
