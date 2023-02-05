a3bfeef417946b50c3b0c9ef967299714a9fd157
TrakaDev <trakadev@gmail.com>
2023-02-04 22:33:53 -0600

    [ADD] Video

---
+++ src/data/video.js
@@ -0,0 +1,15 @@
+export const video_data = {
+    title: {
+        en: "Leading companies trust us",
+        es: "Las mejores empresas confían en nosotros"
+    },
+    substitle1: {
+        en: "WWe believe it's important for everyone to have access to marketing strategies",
+        es: "Creemos que es importante que todos tengan acceso a estrategias de marketing"
+    },
+    substitle2: {
+        en: "as they can help drive the growth of any business, big or small.",
+        es: "ya que pueden ayudar a impulsar el crecimiento de cualquier negocio, ya sea pequeño o grande.",
+    }
+
+}
