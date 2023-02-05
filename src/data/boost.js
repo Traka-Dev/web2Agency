24365c7c7a7cc8ba50370dfb8bf8f07b1bf07400
TrakaDev <trakadev@gmail.com>
2023-02-04 22:16:13 -0600

    [ADD] Boost

---
+++ src/data/boost.js
@@ -0,0 +1,23 @@
+export const boost_data = {
+    title: {
+        en: "Boost your business by choosing us",
+        es: "Mejora tu negocio eligiéndonos"
+    },
+    subtitle: {
+        en: "Create custom landing pages that convert more visitors than any website.",
+        es: "Crea landing pages personalizadas que conviertan más visitantes que cualquier otro sitio web."
+    },
+    item1: {
+        en: "Unlimited design possibility",
+        es: "Posibilidades de diseño ilimitadas"
+    },
+    item2: {
+        en: "Completely responsive features",
+        es: "Características completamente responsivas"
+    },
+    item3: {
+        en: "Growth Hacking",
+        es: "Growth Hacking"
+    }
+
+}
